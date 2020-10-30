14a759b8d823dcaa8c371c430ab526d6d2b95856
DrHyu <cabecej0@gmail.com>
2020-10-30 12:49:51 +0100

    Initial stuff, navbar and router working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchPage from "./pages/Search";
+import LandingPage from "./pages/Landing";
+import NavBar from "./components/nav/NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/search" component={SearchPage} exact />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Landing.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Landing() {
+  return <div>Hope you had a safe landing !</div>;
+}
