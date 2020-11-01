8bdc19970582350357225ea0fa6a914fbe487a2e
DrHyu <cabecej0@gmail.com>
2020-11-01 11:19:12 +0100

    Integrated LazyLoadImage in SearchResult

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 import "./search.css";
+
+const placeHolder =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=";
 
 export default function SearchResult(props) {
   return (
     <div className="container search-result">
       <div className="row">
         <div className="col-4">
-          <img src={props.result.img} alt="Some" />
+          <LazyLoadImage
+            alt={placeHolder}
+            src={props.result.img} // use normal <img> attributes as props
+            effect="opacity"
+          />
         </div>
         <div className="col-8">
           <div className="container">
