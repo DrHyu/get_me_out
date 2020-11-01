bd867438cda2c7c5af1727d095a9739aefedc763
bernat <bernat.huguet@gmail.com>
2020-11-01 21:54:22 +0100

    Prepared dev scripts + gitignore files

---
+++ backend/setup_dev_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+virtualenv -p python3.8 venv
+source venv/bin/activate
+cd gmo_backend
+apt-get -y update  &&  apt-get install -y python3-dev
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate --run-syncdb
+python3 manage.py collectstatic --noinput
