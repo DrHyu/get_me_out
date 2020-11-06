0a68ef1f6341cad9b5c6c287200b167b21d94f76
bernat <bernat.huguet@gmail.com>
2020-11-06 18:11:38 +0100

    Added auth endpoint

--- backend/gmo_backend/gmo_backend/urls.py
+++ backend/gmo_backend/gmo_backend/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gamerooms/', include('gamerooms.urls')),
+    path('users/', include('users.urls')),
 ]
---
+++ backend/gmo_backend/users/urls.py
@@ -0,0 +1,13 @@
+# Base Imports
+
+# Third Party Libraries
+from rest_framework.authtoken import views as authviews
+from django.urls import path
+
+# Project Imports
+#from users.views import *
+
+
+urlpatterns = [
+    path('auth', authviews.obtain_auth_token),
+]
